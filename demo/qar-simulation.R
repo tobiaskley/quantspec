00672275950733d27918b0d1c31d6dc513630c06
tobiaskley <tobias.kley@ruhr-uni-bochum.de>
2014-04-16 23:07:35 +0200

    Changes to the demo and the documentation.

--- demo/qar-simulation.R
+++ demo/qar-simulation.R
@@ -11,11 +11,14 @@
 # Finally, plots for the last simulation run are shown.
 ################################################################################
 
-ts <- ts3
+ts <- ts1
 type <- "copula"
+
+N <- 128
+R <- 100
+
+freq <- 2*pi*(1:16)/32
 levels <- c(0.25, 0.5, 0.75)
-freq <- 2*pi*(1:32)/64
-R <- 1
 
 J <- length(freq)
 K <- length(levels)
@@ -27,33 +30,30 @@ csd <- quantileSD(N=2^8, seed.init = 2581, type = type,
 # init an array for the root integrated mean squared errors
 # (1: CR periodogram, 2: rank-based Laplace periodogram,
 #  3: smoothed CR periodogram, 4: smoothed rank-based Laplace pg.)
-rimse  <- array(0, dim=c(4,J,K,K))
+sims  <- array(0, dim=c(4,R,J,K,K))
 
-weight <- kernelWeight(W=W1, N=32, bw=0.3)
+weight <- kernelWeight(W=W1, bw=0.3)
 
-trueV <- getValues(csd, frequencies=freq)
 for (i in 1:R) {
-  Y <- ts3(64)
-  
+  Y <- ts1(N)
+
   CR <- quantilePG(Y, levels.1=levels, type="clipped")
-  rimse[1,,,] <- rimse[1,,,] +
-      abs(getValues(CR, frequencies=freq)[,,,1]-trueV)^2
-      
-  
+  sims[1,i,,,] <- getValues(CR, frequencies=freq)[,,,1]
+
   LP <- quantilePG(Y, levels.1=levels, type="qr")
-  rimse[2,,,] <- rimse[2,,,] + 
-      abs(getValues(LP, frequencies=freq)[,,,1]-trueV)^2
-  
-  sCR <- smoothedPG(CR, weight=weight) # , frequencies=freq
-  rimse[3,,,] <- rimse[3,,,] + 
-      abs(getValues(sCR, frequencies=freq)[,,,1]-trueV)^2
-  
-  sLP <- smoothedPG(LP, weight=weight) # , frequencies=freq
-  rimse[4,,,] <- rimse[4,,,] + 
-      abs(getValues(sLP, frequencies=freq)[,,,1]-trueV)^2
+  sims[2,i,,,] <- getValues(LP, frequencies=freq)[,,,1]
+
+  sCR <- smoothedPG(CR, weight=weight)
+  sims[3,i,,,] <- getValues(sCR, frequencies=freq)[,,,1]
+
+  sLP <- smoothedPG(LP, weight=weight)
+  sims[4,i,,,] <- getValues(sLP, frequencies=freq)[,,,1]
 }
 
-rimse <- sqrt(apply(rimse, c(1,3,4), mean) / R)
+trueV <- getValues(csd, frequencies=freq)
+
+Sqdeviation <- apply(sims, c(1,2), function(x) {abs(x-trueV)^2})
+rimse <- sqrt(apply(Sqdeviation, c(1,3,4), mean) / R)
 
 # Inspect the rimse, but note that to have reliable approximations of
 # the true rimse the simulations should run much longer.
@@ -63,9 +63,10 @@ for (i in 1:4) {rimse[i,,]}
 # time series of length 64 are not quiet long enought to yield
 # reliable estimators.
 
-plot(sCR, qsd=csd, frequencies = freq)
-plot(sCR, qsd=csd, plotPG=TRUE, frequencies = freq)
-plot(sLP, qsd=csd, frequencies = freq,
+f <- getFrequencies(sCR)
+plot(sCR, qsd=csd, frequencies = f[f > 0])
+plot(sCR, qsd=csd, plotPG=TRUE, frequencies = f[f > 0])
+plot(sLP, qsd=csd, frequencies = f[f > 0],
     ptw.CIs = 0, type.scaling="real-imaginary")
-plot(sLP, qsd=csd, plotPG=TRUE, frequencies = freq,
+plot(sLP, qsd=csd, plotPG=TRUE, frequencies = f[f > 0],
     ptw.CIs = 0, type.scaling="real-imaginary")
