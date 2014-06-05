57611bd89d4402b602411f8c6c3c76a5719fdeb1
tobiaskley <tobias.kley@ruhr-uni-bochum.de>
2014-06-05 22:35:27 +0200

    Bumped version number to 1.0-1. Updated NEWS file.

--- R/quantspec-package.R
+++ R/quantspec-package.R
@@ -11,8 +11,8 @@ NULL
 #'  \tabular{ll}{
 #'    \cr Package: \tab quantspec
 #'    \cr Type:    \tab Package
-#'    \cr Version: \tab 1.0-0.99
-#'    \cr Date:    \tab 2014-04-27
+#'    \cr Version: \tab 1.0-1
+#'    \cr Date:    \tab 2014-06-05
 #'    \cr License: \tab GPL (>= 2)
 #'  }
 #'
