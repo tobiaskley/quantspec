64f28a657914a9cc356e5515b5dcf9d9959639fd
tobiaskley <tobias.kley@ruhr-uni-bochum.de>
2016-11-23 12:10:23 +0000

    Added mvts1--mvts4 that simulate the models from Buecher/Kojadinovic (2016).

--- R/models.R
+++ R/models.R
@@ -224,3 +224,244 @@ ts2 <- function(n){
 
 # Example from Subba Rao's paper
 ts3 <- function(n){ARCH1(n,1/1.9,0.9,innov=function(n){rnorm(n)})}
+
+
+################################################################################
+#' Functions to simulate multivariate time series models from B{\"u}cher and
+#' Kojadinovic (2016)
+#' 
+#' @details
+#' In these bivariate time series models the cross sectional dependence is
+#' introduced via a cross sectional dependence of the innovations. More precisely,
+#' \eqn{(\varepsilon_{i1}, \varepsilon_{i2}) := (\Phi^{-1}(U_{i1}), \Phi^{-1}(U_{i2}))},
+#' with \eqn{\Phi} the c.d.f. of the standard normal distribution and
+#' \eqn{(U_{i1}, U_{i2})} bivariate i.i.d. from either a Clayton copula with
+#' parameters \eqn{1} or \eqn{4} or Gumbel copula \eqn{1.5} or \eqn{3},
+#' respectively.
+#' 
+#' The four models then have the following mechanisms to introduce serial
+#' dependence: 
+#' 
+#' \code{mvts1} bivariate AR(1) model:
+#' \deqn{X_{ij} = 0.5 X_{i-1,j} + \varepsilon_{ij},}
+#'
+#' @name ts-models-mv
+#' @aliases mvts1
+#' @export
+#' 
+#' @importFrom copula mvdc
+#' @importFrom copula archmCopula
+#' @importFrom copula rMvdc
+#' @importFrom zoo zoo
+#' 
+#' @param n     length of the time series to be returned
+#' @param innov flag to indicate whether the independent innovations are
+#'              distributed according to Clayton copula with parameters 1 or 4
+#'              or according to Gumbel copula with parameters 1.5 or 3.
+#'              
+#' @references
+#' B{\"u}cher, A., & Kojadinovic, I. (2016).
+#' A dependent multiplier bootstrap for the sequential empirical copula
+#' process under strong mixing. \emph{Bernoulli}, \bold{22}(2), 927--968.
+#'
+#' @examples
+#' require("zoo")
+#' Y <- zoo(mvts1(100, innov = "Clayton4"))
+#' plot(Y)
+################################################################################
+
+# Example from Axel's paper
+mvts1 <- function(n = 100, innov = c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3")) {
+  overhead <- 100
+  innov <- match.arg(innov, c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3"))[1]
+  switch(innov,
+      "Clayton1" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 1),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Clayton4" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 4),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel15" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 1.5),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel3" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 3),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))}
+  )
+  eps <- rMvdc(n+overhead+1, innovDist)
+  Y <- matrix(0, nrow = n+overhead, ncol = 2)
+  Y[1, ] <- eps[1, ]
+  for (t in 2:(n+overhead)) {
+    Y[t, ] <- 0.5 * Y[t-1, ] + eps[t, ]
+  }
+  Y[(overhead+1):(overhead+n), ]
+}
+
+
+################################################################################
+#' @details
+#' \code{mvts2} bivariate NAR(1) model:
+#' \deqn{X_{ij} = 0.6 sin(X_{i-1,j}) + \varepsilon_{ij},} 
+#'
+#' @name ts-models-mv
+#' @aliases mvts2
+#' @export
+#' 
+#' @importFrom copula mvdc
+#' @importFrom copula archmCopula
+#' @importFrom copula rMvdc
+#' @importFrom zoo zoo
+#' 
+#' @examples
+#' require("zoo")
+#' Y <- zoo(mvts2(100, innov = "Gumbel15"))
+#' plot(Y)
+################################################################################
+
+# Example from Axel's paper
+mvts2 <- function(n = 100, innov = c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3")) {
+  overhead <- 100
+  innov <- match.arg(innov, c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3"))[1]
+  switch(innov,
+      "Clayton1" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 1),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Clayton4" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 4),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel15" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 1.5),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel3" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 3),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))}
+  )
+  eps <- rMvdc(n+overhead+1, innovDist)
+  Y <- matrix(0, nrow = n+overhead, ncol = 2)
+  Y[1, ] <- eps[1, ]
+  for (t in 2:(n+overhead)) {
+    Y[t, ] <- 0.6 * sin(Y[t-1, ]) + eps[t, ]
+  }
+  Y[(overhead+1):(overhead+n), ]
+}
+
+
+################################################################################
+#' @details
+#' \code{mvts3} bivariate EXPAR(1) model:
+#' \deqn{X_{ij} = (0.8 - 1.1 exp(-50 X^2_{i-1,j}) X_{i-1,j} + 0.1 \varepsilon_{ij},} 
+#'
+#' @name ts-models-mv
+#' @aliases mvts3
+#' @export
+#' 
+#' @importFrom copula mvdc
+#' @importFrom copula archmCopula
+#' @importFrom copula rMvdc
+#' @importFrom zoo zoo
+#' 
+#' @examples
+#' require("zoo")
+#' Y <- zoo(mvts3(100, innov = "Gumbel15"))
+#' plot(Y)
+################################################################################
+
+# Example from Axel's paper
+mvts3 <- function(n = 100, innov = c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3")) {
+  overhead <- 100
+  innov <- match.arg(innov, c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3"))[1]
+  switch(innov,
+      "Clayton1" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 1),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Clayton4" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 4),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel15" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 1.5),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel3" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 3),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))}
+  )
+  eps <- rMvdc(n+overhead+1, innovDist)
+  Y <- matrix(0, nrow = n+overhead, ncol = 2)
+  Y[1, ] <- eps[1, ]
+  for (t in 2:(n+overhead)) {
+    Y[t, ] <- (0.8 - 1.1 * exp(-50 * Y[t-1, ]^2)) * Y[t-1, ] + 0.1 * eps[t, ]
+  }
+  Y[(overhead+1):(overhead+n), ]
+}
+
+################################################################################
+#' @details
+#' \code{mvts4} bivariate GARCH(1,1) model:
+#' \deqn{X_{ij} = \sigma_{ij} \varepsilon_{ij}}
+#' \deqn{\varepsilon_{i1}^2 = 0.012 + 0.919 \varepsilon_{i-1,1}^2 + 0.072 \varepsilon_{i-1,1}^2,}
+#' #' and
+#' \deqn{\varepsilon_{i2}^2 = 0.037 + 0.868 \varepsilon_{i-1,2}^2 + 0.115 \varepsilon_{i-1,2}^2.}
+#' 
+#' @name ts-models-mv
+#' @aliases mvts4
+#' @export
+#' 
+#' @importFrom copula mvdc
+#' @importFrom copula archmCopula
+#' @importFrom copula rMvdc
+#' @importFrom zoo zoo
+#' 
+#' @examples
+#' require("zoo")
+#' Y <- zoo(mvts4(100, innov = "Gumbel15"))
+#' plot(Y)
+################################################################################
+
+# Example from Axel's paper
+mvts4 <- function(n = 100, innov = c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3")) {
+  overhead <- 100
+  innov <- match.arg(innov, c("Clayton1", "Clayton4", "Gumbel15", "Gumbel3"))[1]
+  switch(innov,
+      "Clayton1" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 1),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Clayton4" = {
+        innovDist <- mvdc(copula = archmCopula(family = "clayton", dim = 2, param = 4),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel15" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 1.5),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))},
+      "Gumbel3" = {
+        innovDist <- mvdc(copula = archmCopula(family = "gumbel", dim = 2, param = 3),
+            margins = c("norm", "norm"),
+            paramMargins = list(list(mean = 0, sd = 1), list(mean = 0, sd = 1)))}
+  )
+  eps <- rMvdc(n+overhead+1, innovDist)
+  Y <- matrix(0, nrow = n+overhead, ncol = 2)
+  sigma1 <- sqrt( 0.012 / (1 - 0.919 - 0.072) )
+  sigma2 <- sqrt( 0.037 / (1 - 0.868 - 0.115) )
+
+  Y[1, 1] <- sigma1 * eps[1, 1]
+  Y[1, 2] <- sigma2 * eps[1, 2]
+  for (t in 2:(n+overhead)) {
+    sigma1 <- 0.012 + 0.919 * sigma1 + 0.072 * eps[t-1,1]^2
+    sigma2 <- 0.037 + 0.868 * sigma2 + 0.115 * eps[t-1,2]^2
+    Y[t, 1] <- sigma1 * eps[t, 1]
+    Y[t, 2] <- sigma2 * eps[t, 2]
+  }
+  Y[(overhead+1):(overhead+n), ]
+}
